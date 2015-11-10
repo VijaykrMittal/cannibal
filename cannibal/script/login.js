b917fe53965f1c61188a6134b8e3965e740af539
tester user <testeruserjmd@gmail.com>
2015-11-10 12:22:07 +0000

    Changes made by tester user (testeruserjmd@gmail.com).

--- cannibal/script/login.js
+++ cannibal/script/login.js
@@ -4,6 +4,8 @@
     
     loginViewMOdel = kendo.data.ObservableObject.extend({
         
+        log_email:'',
+        log_pwd:'',
         show:function()
         {
             
@@ -26,8 +28,44 @@
             });
             
             $('#signin').click(function(){
-                app.mobileApp.navigate('views/shop.html');    
+               // app.mobileApp.navigate('views/shop.html');    
+                app.loginService.viewModel.loginValidation();
             });
+        },
+        
+        // Validation Function for Login
+        loginValidation : function()
+        {
+            var that = this,
+                username = that.get('log_email'),
+                password = that.get('log_pwd');
+            
+            var dataParam = [];
+            //console.log(navigator.notification.alert);
+            if(username === "")
+            {
+                navigator.notification.alert('Please enter Username/Email',function(){},'Notification','OK');
+            }
+            else if(password === "")
+            {
+                navigator.notification.alert('Please enter Password',function(){},'Notification','OK');
+            }
+            else
+            {
+                dataParam['userName'] = username;
+                dataParam['pass'] = password;
+                console.log(dataParam);
+                
+                var loginDataSource = new kendo.data.DataSource({
+                    transport:{
+                        read:{
+                            url:'',
+                            type:'GET',
+                            dataType:''
+                        }
+                    }
+                });
+            }
         },
         
         moveToForgetPwd:function()
